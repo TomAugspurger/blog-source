8c7f16299dc6c3f77b699289cb3376c01652dd4e
TomAugspurger <thomas-augspurger@uiowa.edu>
2014-02-21 13:00:06 -0600

    add quiz 3 review

--- pelicanconf.py
+++ pelicanconf.py
@@ -75,7 +75,7 @@ TRANSLATION_FEED_ATOM = None
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 # Search
 SEARCH_BOX = True
